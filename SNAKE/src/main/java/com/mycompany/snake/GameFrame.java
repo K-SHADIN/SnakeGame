6a77cd5f8bac3511b940cfbcbcb3591ef88e1c71
K-SHADIN <64790133+K-SHADIN@users.noreply.github.com>
2022-01-20 00:14:46 +0600

    JavaSwing Project

---
+++ SNAKE/src/main/java/com/mycompany/snake/GameFrame.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.snake;
+
+/**
+ *
+ * @author shadin
+ */
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame{
+
+	GameFrame(){
+			
+		this.add(new GamePanel());
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+		
+	}
+}
---
+++ SNAKE/src/main/java/com/mycompany/snake/SnakeGame.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.snake;
+
+/**
+ *
+ * @author shadin
+ */
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		
+		new GameFrame();
+	}
+
+    
+
+    
+}
